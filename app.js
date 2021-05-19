d6de5bbcdb1c33a1a6629b9013a319a0f5c89df4
Nithin Sai K J <nithins674@gmail.com>
2021-05-19 23:14:53 +0530

    game logic with human player

---
+++ app.js
@@ -0,0 +1,84 @@
+var origiBoard;
+const huPlayer = '0';
+const aiPlayer = 'X';
+
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cells = document.querySelectorAll('.cell');
+
+startGame();
+
+
+//  function to start game
+function startGame() {
+    document.querySelector('.endgame').style.display = "none";
+    origiBoard = Array.from(Array(9).keys());
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = "";
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick);
+    }
+}
+
+
+// turnClick can be called by both human player & ai
+function turnClick(square) {
+    turn(square.target.id, huPlayer)
+}
+
+
+// actual code to add X & O to the board 
+// also checks foe winner
+function turn(squareId, player) {
+    origiBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+
+    let gameWon = checkWin(origiBoard, player);
+
+    if (gameWon) gameOver(gameWon);
+}
+
+
+// function to check winner
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a, []); // adds index of board if the player has played it
+
+    let gameWon = null;
+
+    for (let [index, win] of winCombos.entries()) {
+        if (win.every(ele => plays.indexOf(ele) > -1)) { // has the player played in every possible win in winCombos
+            gameWon = {
+                index: index, // index of the winCombo
+                player: player, // player who played that win
+            };
+            break;
+        }
+    }
+    return gameWon;
+}
+
+
+// gameover function
+function gameOver(gameWon) {
+    for (const index of winCombos[gameWon.index]) {
+        let d = document.getElementById(index)
+        d.style.backgroundColor =
+            gameWon.player == huPlayer ? '#4078c0' : 'red';
+        d.style.color = "white"
+    }
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false)
+    }
+}
